d44a044bdf60b9d5abf31e41ac5c9bfbe3407458
WEIKAI HSU <N26104890@gs.ncku.edu.tw>
2021-12-30 19:06:28 +0800

    VM Workinggit add --all

--- main/scala/aias_lab2/VendingMachine.scala
+++ main/scala/aias_lab2/VendingMachine.scala
@@ -3,21 +3,31 @@ package aias_lab2
 import chisel3._
 import chisel3.util._
 
-class VendingMachine extends Module{
+class VendingMachine(Prices:Seq[Int]) extends Module{
+  
+  val pricesReg = RegInit(VecInit(Prices.map(x=>x.U(8.W))))
+
   val io =  IO(new Bundle{
-      val out = Output(Bool())
+    val Drinks_button = Input(Vec(Prices.length,Bool()))
+    val Coin = Input(UInt(8.W))
+    val Drinks_light = Output(Vec(Prices.length,Bool()))
+    val out = Output(Bool())
   })
 
-  def pwm(nrCycles:Int , din:UInt) = {
-      val cntReg = RegInit(0.U(unsignedBitLength(nrCycles-1).W))
-      cntReg := Mux(cntReg === (nrCycles-1).U , 0.U , cntReg + 1.U)
-      din > cntReg
-  }
+  val Balance = RegInit(0.U(8.W))
+  Balance := Balance + io.Coin
 
-  val din = 3.U
-  io.out := pwm(10,din)
+//   def pwm(nrCycles:Int , din:UInt) = {
+//       val cntReg = RegInit(0.U(unsignedBitLength(nrCycles-1).W))
+//       cntReg := Mux(cntReg === (nrCycles-1).U , 0.U , cntReg + 1.U)
+//       din > cntReg
+//   }
+
+//   val din = 3.U
+  io.out := false.B
 }
 
 object VMDriver extends App{
-    Driver.execute(args,()=>new VendingMachine)
+  val Prices = Seq(30,40,50)
+  Driver.execute(args,()=>new VendingMachine(Prices))
 }
