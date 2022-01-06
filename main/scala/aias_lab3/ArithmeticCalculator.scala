e582d035e796da14ffebaebaa16108354844967d
Wei-Kai Hsu <N26104890@gs.ncku.edu.tw>
2022-01-07 02:43:52 +0800

    new idea come up

--- main/scala/aias_lab3/ArithmeticCalculator.scala
+++ main/scala/aias_lab3/ArithmeticCalculator.scala
@@ -3,61 +3,81 @@ package aias_lab3
 import chisel3._
 import chisel3.util._
 import scala.languageFeature.postfixOps
+import scala.annotation.switch
 
 class ArithmeticCalculator extends Module{
   val io = IO(new Bundle{
     val key_in = Input(UInt(4.W))
     val valid = Output(Bool())
     val Answer = Output(UInt(32.W))
+    val RegCheck = Output(Vec(32,UInt(32.W)))
   })
+  
+  //stack declaration
+  val stack = Module(new Stack(4))
+  //default
+  stack.io.push := false.B
+  stack.io.pop := false.B
+  stack.io.dataIn := 0.U
 
-  val stack = Module(new stack(4))
   val inReg = RegInit(0.U(32.W))
 
-  val length = 16
+  val length = 32
   val postfix = RegInit(VecInit(Seq.fill(length)(0.U(32.W))))
-  val pointer = RegInit(0.U(log2Ceil(length).W))
+  val is_num = RegInit(VecInit(Seq.fill(length)(false.B)))
+  val ptr = RegInit(0.U(log2Ceil(length).W))
   
-  //0~9
+  val cnt = RegInit(0.U(4.W))
+  cnt := Mux(cnt === 9.U, 0.U , cnt + 1.U)
+  val tick = WireDefault(false.B)
+  tick := cnt === 1.U
+
+  
+  // //0~9
   val number = Wire(Bool())
   number := io.key_in < 10.U
-  //+,-,x
+  // //+,-,x
   val operator = Wire(Bool())
   operator := (io.key_in >= 10.U) || (io.key_in <= 12.U)
-  //(,) parentheses
+  // //(,) parentheses
   val parentheses = Wire(Bool())
   parentheses := (io.key_in >= 13.U) || (io.key_in <= 14.U)
   //= equal sign
   val equal = Wire(Bool())
   equal := io.key_in === 15.U
   
-  //operand priority
-  val priority = WireDefault(false.B)
-  when(io.key === 12.U){
-    priority := true.B
+  
+  // operand Precedence
+  def op_precedence (in:UInt):UInt{
+    
   }
 
-  when(number){
-    inReg := (inReg << 3) + (inReg << 1) + io.key_in
-  }.elsewhen(operator){
-    //put operand in postfix
-    postfix(pointer) := inReg
-    pointer := pointer + 1
-    inReg := 0
-    
-    //operand
-    when(stack.dataOut(4) > io.key_in(4))
-    
-    stack.push := true.B
-    stack.dataIn := io.key_in
-  }.elsewhen(parentheses){
+  when(tick){
+    when(number){
+      inReg := (inReg << 3) + (inReg << 1) + io.key_in
+    }.otherwise{
+      postfix(ptr) := inReg
+      is_num(ptr) := true.B
+      ptr := ptr + 1.U
+      inReg := 0.U
+    }
 
-  }.elsewhen(equal){
-     
+    when(operator || parentheses){
+      stack.io.push := true.B
+      stack.io.dataIn := io.key_in
+    }.elsewhen(equal){
+
+    }
   }
+  
+    
 
   io.valid := false.B
   io.Answer := 0.U
+
+  for(i <- 0 until length){
+    io.RegCheck(i) := postfix(i)
+  }
 }
 
 object ACDriver extends App{
---
+++ test/scala/aias_lab3/ArithmeticCalculator_test.scala
@@ -0,0 +1,18 @@
+package aias_lab3
+
+import chisel3.iotesters.{Driver,PeekPokeTester}
+
+class ArithmeticCalculatorTests (dut : ArithmeticCalculator) extends PeekPokeTester(dut){
+  val Dict = Map(('0',0),('1',1),('2',2),('3',3),('4',4),('5',5),('6',6),('7',7),('8',8),('9',9),('+',10),('-',11),('*',12),('(',13),(')',14),('=',15))
+  val formular = "13*(25+416)="
+
+  for(i <- 0 until formular.length){
+    poke(dut.io.key_in,Dict(formular(i)))
+    step(10)
+  }
+}
+object ACTester extends App{
+  Driver.execute(args,() => new ArithmeticCalculator()){
+    c => new  ArithmeticCalculatorTests(c)
+  }
+}
